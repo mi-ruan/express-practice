5de6a66f5306e8b1377d917066f6cfac5df3e980
Michael Ruan <michaelruan1@gmail.com>
2018-05-14 15:34:51 -0400

    add routes and controllers

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var catalog = require('./routes/catalog');
+var wiki = require('./wiki.js');
 
 var app = express();
 
@@ -28,8 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
+app.use('/catalog', catalog)
+app.use('/wiki', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
